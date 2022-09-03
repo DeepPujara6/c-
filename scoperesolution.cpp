85e5cc0f71b424ec90dced394195232ba8ecb154
DeepPujara6 <111839933+DeepPujara6@users.noreply.github.com>
2022-09-03 14:43:02 +0530

    Update scoperesolution.cpp

--- scoperesolution.cpp
+++ scoperesolution.cpp
@@ -1,9 +1,10 @@
+// scope resolution operator= ::
 #include<iostream>
 using namespace std;
 int x=10;
 int main()
 {
 	int x=20;
-	cout << "value of gobal X=" << ::x << endl;
+	cout << "value of global X=" << ::x << endl;
 	cout << "value of local X=" << x;
 }
